b6819a4b2207ab08991f29bbee14dcff17c199e0
caolegileunardopdm-byte <caolegileunardo.pdm@gmail.com>
2026-09-20 08:23:05 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+// app.use(
+//   cors({
